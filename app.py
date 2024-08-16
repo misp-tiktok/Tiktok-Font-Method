6ba4c73bbcd6239ab3872d1005a6cf5033dd9316
misp-tiktok <mispfont@outlook.com>
2024-08-16 17:14:07 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from hashlib import md5
 import random
 import time
 import requests
+from copy import deepcopy
 
 app = Flask(__name__)
 
@@ -13,29 +14,16 @@ class XG:
         self.hex_CE0 = [0x05, 0x00, 0x50, random.choice(range(0, 0xFF)), 0x47, 0x1e, 0x00, random.choice(range(0, 0xFF)) & 0xf0]
 
     def addr_BA8(self):
-        tmp = ''
-        hex_BA8 = []
-        for i in range(0x0, 0x100):
-            hex_BA8.append(i)
-        for i in range(0, 0x100):
-            if i == 0:
-                A = 0
-            elif tmp:
-                A = tmp
-            else:
-                A = hex_BA8[i - 1]
+        tmp = None
+        hex_BA8 = list(range(0x100))
+        for i in range(0x100):
+            A = tmp if tmp else hex_BA8[i - 1] if i > 0 else 0
             B = self.hex_CE0[i % 0x8]
-            if A == 0x05:
-                if i != 1:
-                    if tmp != 0x05:
-                        A = 0
+            if A == 0x05 and i != 1 and tmp != 0x05:
+                A = 0
             C = A + i + B
-            while C >= 0x100:
-                C = C - 0x100
-            if C < i:
-                tmp = C
-            else:
-                tmp = ''
+            C %= 0x100
+            tmp = C if C < i else None
             D = hex_BA8[C]
             hex_BA8[i] = D
         return hex_BA8
@@ -45,22 +33,16 @@ class XG:
         tmp_hex = deepcopy(hex_BA8)
         for i in range(self.length):
             A = debug[i]
-            if not tmp_add:
-                B = 0
-            else:
-                B = tmp_add[-1]
+            B = tmp_add[-1] if tmp_add else 0
             C = hex_BA8[i + 1] + B
-            while C >= 0x100:
-                C = C - 0x100
+            C %= 0x100
             tmp_add.append(C)
             D = tmp_hex[C]
             tmp_hex[i + 1] = D
             E = D + D
-            while E >= 0x100:
-                E = E - 0x100
+            E %= 0x100
             F = tmp_hex[E]
-            G = A ^ F
-            debug[i] = G
+            debug[i] = A ^ F
         return debug
 
     def calculate(self, debug):
@@ -71,111 +53,85 @@ class XG:
             D = B ^ C
             E = RBIT(D)
             F = E ^ self.length
-            G = ~F
-            while G < 0:
-                G += 0x100000000
-            H = int(hex(G)[-2:], 16)
-            debug[i] = H
+            G = ~F & 0xFF
+            debug[i] = G
         return debug
 
     def main(self):
-        result = ''
-        for item in self.calculate(self.initial(self.debug, self.addr_BA8())):
-            result = result + hex_string(item)
-
-        return '8404{}{}{}{}{}'.format(hex_string(self.hex_CE0[7]), hex_string(self.hex_CE0[3]),
-                                       hex_string(self.hex_CE0[1]), hex_string(self.hex_CE0[6]), result)
+        result = ''.join(hex_string(item) for item in self.calculate(self.initial(self.debug, self.addr_BA8())))
+        return f'8404{hex_string(self.hex_CE0[7])}{hex_string(self.hex_CE0[3])}{hex_string(self.hex_CE0[1])}{hex_string(self.hex_CE0[6])}{result}'
 
 def hex_string(num):
-    tmp_string = hex(num)[2:]
-    if len(tmp_string) < 2:
-        tmp_string = '0' + tmp_string
-    return tmp_string
+    return f'{num:02x}'
 
 def RBIT(num):
-    result = ''
-    tmp_string = bin(num)[2:]
-    while len(tmp_string) < 8:
-        tmp_string = '0' + tmp_string
-    for i in range(0, 8):
-        result = result + tmp_string[7 - i]
-    return int(result, 2)
+    return int(f'{num:08b}'[::-1], 2)
 
 def reverse(num):
-    tmp_string = hex(num)[2:]
-    if len(tmp_string) < 2:
-        tmp_string = '0' + tmp_string
-    return int(tmp_string[1:] + tmp_string[:1], 16)
+    tmp_string = f'{num:02x}'
+    return int(tmp_string[1] + tmp_string[0], 16)
 
 def run(param="", stub="", cookie=""):
     gorgon = []
     ttime = time.time()
     Khronos = hex(int(ttime))[2:]
-    url_md5 = md5(bytearray(param, 'utf-8')).hexdigest()
-    for i in range(0, 4):
-        gorgon.append(int(url_md5[2 * i: 2 * i + 2], 16))
+    url_md5 = md5(param.encode('utf-8')).hexdigest()
+    gorgon.extend(int(url_md5[i:i+2], 16) for i in range(0, 8, 2))
+    
     if stub:
         data_md5 = stub
-        for i in range(0, 4):
-            gorgon.append(int(data_md5[2 * i: 2 * i + 2], 16))
+        gorgon.extend(int(data_md5[i:i+2], 16) for i in range(0, 8, 2))
     else:
-        for i in range(0, 4):
-            gorgon.append(0x0)
+        gorgon.extend([0x0] * 4)
+
     if cookie:
-        cookie_md5 = md5(bytearray(cookie, 'utf-8')).hexdigest()
-        for i in range(0, 4):
-            gorgon.append(int(cookie_md5[2 * i: 2 * i + 2], 16))
+        cookie_md5 = md5(cookie.encode('utf-8')).hexdigest()
+        gorgon.extend(int(cookie_md5[i:i+2], 16) for i in range(0, 8, 2))
     else:
-        for i in range(0, 4):
-            gorgon.append(0x0)
-    gorgon = gorgon + [0x1, 0x1, 0x2, 0x4]
-    for i in range(0, 4):
-        gorgon.append(int(Khronos[2 * i: 2 * i + 2], 16))
+        gorgon.extend([0x0] * 4)
+
+    gorgon += [0x1, 0x1, 0x2, 0x4]
+    gorgon.extend(int(Khronos[i:i+2], 16) for i in range(0, 8, 2))
+    
     return {'X-Gorgon': XG(gorgon).main(), 'X-Khronos': str(int(ttime))}
 
 def get_stub(data):
     if isinstance(data, dict):
         data = json.dumps(data)
-
     if isinstance(data, str):
-        data = data.encode(encoding='utf-8')
-    if data == None or data == "" or len(data) == 0:
+        data = data.encode('utf-8')
+    
+    if not data:
         return "00000000000000000000000000000000"
 
     m = md5()
     m.update(data)
-    res = m.hexdigest()
-    res = res.upper()
-    return res
+    return m.hexdigest().upper()
 
 def get_profile(session_id, device_id, iid):
-    """Retrieve the current TikTok username for a given session, device, and iid."""
+    url = f"https://api.tiktokv.com/passport/account/info/v2/?id=kaa&version_code=34.0.0&language=en&app_name=lite&app_version=34.0.0&carrier_region=SA&device_id={device_id}&tz_offset=10800&mcc_mnc=42001&locale=en&sys_region=SA&aid=473824&screen_width=1284&os_api=18&ac=WIFI&os_version=17.3&app_language=en&tz_name=Asia/Riyadh&carrier_region1=SA&build_number=340002&device_platform=iphone&iid={iid}&device_type=iPhone13,4"
+    headers = {
+        "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+        "Cookie": f"sessionid={session_id}",
+        "sdk-version": "2",
+        "user-agent": "com.zhiliaoapp.musically/432424234 (Linux; U; Android 5; en; Build/PI;tt-ok/3.12.13.1)",
+    }
     try:
-        url = f"https://api.tiktokv.com/passport/account/info/v2/?id=kaa&version_code=34.0.0&language=en&app_name=lite&app_version=34.0.0&carrier_region=SA&device_id={device_id}&tz_offset=10800&mcc_mnc=42001&locale=en&sys_region=SA&aid=473824&screen_width=1284&os_api=18&ac=WIFI&os_version=17.3&app_language=en&tz_name=Asia/Riyadh&carrier_region1=SA&build_number=340002&device_platform=iphone&iid={iid}&device_type=iPhone13,4"
-        headers = {
-            "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
-            "Cookie": f"sessionid={session_id}",
-            "sdk-version": "2",
-            "user-agent": "com.zhiliaoapp.musically/432424234 (Linux; U; Android 5; en; fewfwdw; Build/PI;tt-ok/3.12.13.1)",
-        }
-        
-        response = requests.get(url, headers=headers, cookies={"sessionid": session_id})
-        return response.json()["data"]["username"]
-    except Exception as e:
-        return "None"
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()
+        return response.json().get("data", {}).get("username", "None")
+    except requests.RequestException as e:
+        return f"Error: {str(e)}"
 
 def check_is_changed(last_username, session_id, device_id, iid):
-    """Check if the username has been changed in the TikTok profile."""
     return get_profile(session_id, device_id, iid) != last_username
 
 def change_username(session_id, device_id, iid, last_username, new_username):
-    """Attempt to change a TikTok username."""
-    data = f"aid=364225&unique_id={quote(new_username)}"
-    parm = f"aid=364225&residence=&device_id={device_id}&version_name=1.1.0&os_version=17.4.1&iid={iid}&app_name=tiktok_snail&locale=en&ac=4G&sys_region=SA&version_code=1.1.0&channel=App%20Store&op_region=SA&os_api=18&device_brand=iPad&idfv=16045E07-1ED5-4350-9318-77A1469C0B89&device_platform=iPad&device_type=iPad13,4&carrier_region1=&tz_name=Asia/Riyadh&account_region=sa&build_number=11005&tz_offset=10800&app_language=en&carrier_region=&current_region=&aid=364225&mcc_mnc=&screen_width=1284&uoo=1&content_language=&language=en&cdid=B75649A607DA449D8FF2ADE97E0BC3F1&openudid=7b053588b18d61b89c891592139b68d918b44933&app_version=1.1.0"
+    data = f"aid=364225&unique_id={new_username}"
+    param = f"aid=364225&device_id={device_id}&iid={iid}&app_name=tiktok_snail"
     
-        
-    sig = run(parm, md5(data.encode("utf-8")).hexdigest() if data else None,None)  
-    url = f"https://api.tiktokv.com/aweme/v1/commit/user/?{parm}"
+    sig = run(param, get_stub(data), None)
+    url = f"https://api.tiktokv.com/aweme/v1/commit/user/?{param}"
 
     headers = {
         "cookie": f"sessionid={session_id}",
@@ -185,7 +141,7 @@ def change_username(session_id, device_id, iid, last_username, new_username):
         "user-agent": "com.zhiliaoapp.musically/2026902040 (Linux; U; Android 8.1.0; en; iPhone13,4; Build/OPM2.171019.012;tt-ok/3.10.1.2)",
     }
     response = requests.post(url, data=data, headers=headers)
-    print(response.content)
+    
     if response.status_code == 200:
         if check_is_changed(last_username, session_id, device_id, iid):
             return f"Success! Your username has been changed to {new_username}."
@@ -204,10 +160,9 @@ def change_username_route():
     session_id = data.get('sessionId')
     new_username = data.get('newUsername')
 
-    # Replace these placeholders with actual values
     device_id = "your_device_id"
     iid = "your_iid"
-    last_username = get_profile(session_id, device_id, iid) # This fetches the current username
+    last_username = get_profile(session_id, device_id, iid)
     
     if not last_username or last_username == "None":
         return jsonify({"message": "Error: Unable to retrieve current username. Check session ID."})
